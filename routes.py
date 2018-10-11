93a4027b63857529baabd285530a686991457383
Nodja <me@nodja.com>
2018-10-11 04:01:06 -0400

    add routes and dangobango

--- routes.py
+++ routes.py
@@ -1,23 +1,17 @@
 from aiohttp import web
 
 
-async def index(request):
-    with open('html/index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-
-
-async def joevotes(request):
-    with open('html/joevotes.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-
-
-async def streamschedule(request):
-    with open('html/streamschedule.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
+def add_route(filename):
+    async def func(request):
+        with open(filename) as f:
+            return web.Response(text=f.read(), content_type='text/html')
+    return func
 
 
 def apply_routes(app):
-    app.router.add_get('/', index)
-    app.router.add_get('/joevotes', joevotes)
-    app.router.add_get('/streamschedule', streamschedule)
+    app.router.add_get('/', add_route('html/index.html'))
+    app.router.add_get('/joevotes', add_route('html/joevotes.html'))
+    app.router.add_get('/streamschedule', add_route('html/streamschedule.html'))
+    app.router.add_get('/dangobango', add_route('html/dangobango.html'))
+    app.router.add_get('/dangobango-popup', add_route('html/dangobango-popup.html'))
     app.router.add_static('/static', 'static')
